4654fdc6d4645691e634f2187fc0f48ea4decdb7
Arif6474 <arifulislam64743@gmail.com>
2022-09-03 21:05:43 +0600

    changed done

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
-// const user = require("./public/user.json");
 const usersRoute = require("./route/v1/users.routes");
 const app = express();
-const port = 5000;
+const port = 6000;
 
 app.use(express.text());
 app.use(express.json());
